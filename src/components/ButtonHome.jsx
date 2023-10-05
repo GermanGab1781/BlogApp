1783eeadcda8d991c3f2266f53318db7ecec75c4
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-10-05 02:19:14 -0300

    Small bug removed on Home page

--- src/components/ButtonHome.jsx
+++ src/components/ButtonHome.jsx
@@ -11,9 +11,9 @@ const ButtonHome = ({ text, path, delay }) => {
 
   return (
     <motion.div variants={item} initial="hidden" animate="show" transition={{ duration: 0.3, delay: delay }} className='cursor-pointer group'>
-      <motion.NavLink className='flex p-10 bg-red-800 hover:bg-indigo-700 border border-blue-500 hover:border-slate-50 w-56 group-hover:scale-110 group-hover:rounded-2xl rounded-md transition-all ease-in-out duration-100 delay-[45ms]' to={path}>
+      <NavLink className='flex p-10 bg-red-800 hover:bg-indigo-700 border border-blue-500 hover:border-slate-50 w-56 group-hover:scale-110 group-hover:rounded-2xl rounded-md transition-all ease-in-out duration-100 delay-[45ms]' to={path}>
         <span className='m-auto font-semibold group-hover:font-bold'>{text}</span>
-      </motion.NavLink>
+      </NavLink>
     </motion.div>
   );
 }
